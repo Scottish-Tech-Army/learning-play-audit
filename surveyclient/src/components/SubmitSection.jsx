5e10db4ec1f09a83640fb4f14695347bc9e56e9c
Donal Stewart <covidresponse@demonsoft.org>
2020-12-12 22:58:58 +0000

    Added upload progress indicator

--- surveyclient/src/components/SubmitSection.jsx
+++ surveyclient/src/components/SubmitSection.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 import { useSelector } from "react-redux";
 import { v4 as uuidv4 } from "uuid";
@@ -7,13 +7,25 @@ import { Auth } from "aws-amplify";
 import { SUBMIT } from "./FixedSectionTypes";
 import SectionBottomNavigation from "./SectionBottomNavigation";
 import { SIGNED_IN } from "../model/AuthStates";
+import Modal from "@material-ui/core/Modal";
+
+export const SUBMITTING_START = "Uploading survey response";
+export const SUBMITTING_PHOTOS = "Uploading photos";
+export const SUBMITTING_CONFIRM = "Confirming upload";
+export const SUBMIT_COMPLETE = "Upload complete";
+export const SUBMIT_FAILED = "Upload failed - please try again";
 
 function submitSurvey(request) {
   console.log("POST survey", request);
   return API.post("ltlClientApi", "/survey", request);
 }
 
-async function uploadPhoto(photoId, uploadUrl, photos) {
+async function uploadPhoto(
+  photoId,
+  uploadUrl,
+  photos,
+  progressIncrementCallback
+) {
   console.log("uploadPhoto", photoId, uploadUrl);
 
   const photo = photos[photoId];
@@ -32,6 +44,7 @@ async function uploadPhoto(photoId, uploadUrl, photos) {
   console.log("uploadPhoto response", response);
   if (response.ok) {
     console.log("uploadPhoto succeeded");
+    progressIncrementCallback();
     return Promise.resolve("uploadPhoto succeeded");
   } else {
     console.log("uploadPhoto failed");
@@ -39,13 +52,20 @@ async function uploadPhoto(photoId, uploadUrl, photos) {
   }
 }
 
-function uploadPhotos({ uploadUrls }, photos) {
+function uploadPhotos({ uploadUrls }, photos, progressIncrementCallback) {
   console.log("uploadPhotos", uploadUrls);
   const photoIds = Object.keys(uploadUrls);
   console.log("photoIDs", photoIds);
 
   return Promise.all(
-    photoIds.map((photoId) => uploadPhoto(photoId, uploadUrls[photoId], photos))
+    photoIds.map((photoId) =>
+      uploadPhoto(
+        photoId,
+        uploadUrls[photoId],
+        photos,
+        progressIncrementCallback
+      )
+    )
   );
 }
 
@@ -58,6 +78,11 @@ function SubmitSection({ sections, setCurrentSection }) {
   const state = useSelector((state) => state);
   const authState = useSelector((state) => state.authentication.state);
   const user = useSelector((state) => state.authentication.user);
+
+  const [submitState, setSubmitState] = useState(null);
+  const [submitProgress, setSubmitProgress] = useState(0);
+
+  const incrementProgress = () => setSubmitProgress((progress) => progress + 1);
 
   async function uploadResults() {
     console.log("Results:");
@@ -91,24 +116,36 @@ function SubmitSection({ sections, setCurrentSection }) {
 
     const confirmRequest = { ...requestTemplate };
 
+    setSubmitState(SUBMITTING_START);
+    setSubmitProgress(0);
+
     submitSurvey(submitRequest)
       .then((response) => {
         console.log("submitSurvey complete", response);
+        incrementProgress();
         confirmRequest.body.confirmId = response.confirmId;
-        return uploadPhotos(response, state.photos);
+        setSubmitState(SUBMITTING_PHOTOS);
+        return uploadPhotos(response, state.photos, incrementProgress);
       })
       .then((response) => {
         console.log("uploadPhotos complete", response);
+        setSubmitState(SUBMITTING_CONFIRM);
         return confirmsurvey(confirmRequest);
       })
       .then((response) => {
         console.log("confirmsurvey complete", response);
+        incrementProgress();
+        setSubmitState(SUBMIT_COMPLETE);
       })
       .catch((err) => {
         console.log("Error");
         console.log(err);
+        setSubmitState(SUBMIT_FAILED);
       });
   }
+
+  const maxProgressCount = Object.keys(state.photos).length + 2;
+  const progressValue = Math.round((submitProgress * 100) / maxProgressCount);
 
   return (
     <div className="submit-section">
@@ -124,6 +161,48 @@ function SubmitSection({ sections, setCurrentSection }) {
         currentSectionId={SUBMIT}
         setCurrentSectionId={setCurrentSection}
       />
+      {submitState !== null && (
+        <Modal
+          container={
+            window !== undefined ? () => window.document.body : undefined
+          }
+          keepMounted={false}
+          disableBackdropClick={true}
+          disableEscapeKeyDown={true}
+          open={submitState !== null}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <div className="submit-dialog" aria-labelledby="form-dialog-title">
+            <h2 className="title">Uploading Survey Response</h2>
+            {submitState !== SUBMIT_COMPLETE &&
+              submitState !== SUBMIT_FAILED && <p>Please wait...</p>}
+            {submitState === SUBMIT_COMPLETE && (
+              <p>Thank you for completing the survey</p>
+            )}
+            <div className="progress-bar">
+              <div
+                className="progress-bar-active"
+                style={{ width: progressValue + "%" }}
+              />
+            </div>
+            <div className="submission-status">{submitState}</div>
+            {(submitState === SUBMIT_COMPLETE ||
+              submitState === SUBMIT_FAILED) && (
+              <button
+                className="save-photos-button"
+                onClick={() => setSubmitState(null)}
+                aria-label="Done"
+              >
+                Done
+              </button>
+            )}
+          </div>
+        </Modal>
+      )}
     </div>
   );
 }
